7b50d0759af484cb9f9edad191d2842f2177d359
Septian Adi Pratama <septianadip7998@gmail.com>
2024-08-13 13:48:19 +0700

    form input

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,36 @@
 import * as React from "react";
 
 export default function Form() {
-  return <div>Hello Form</div>;
+  return (
+    <>
+      <h1>Welcome Back</h1>
+      <p>Welcome back! Please enter your details.</p>
+      <div>
+        <div>
+          <label>Email</label>
+          <input
+            className=""
+            placeholder="Enter your email"
+            type="email"
+          />
+        </div>
+      </div>
+      <div>
+        <div>
+          <label>Password</label>
+          <input
+            className=""
+            placeholder="Enter your password"
+            type="password"
+          />
+        </div>
+      </div>
+      <div>
+        <div>
+          <input type="checkbox" name="" id="remember" />
+          <label for="remember">Remember for 30 days</label>
+        </div>
+      </div>
+    </>
+  );
 }
