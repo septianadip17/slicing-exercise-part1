2af0fb5f7c95e015f430940a695fc41811f652a7
Septian Adi Pratama <septianadip7998@gmail.com>
2024-08-13 14:35:14 +0700

    button

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,35 +2,50 @@ import * as React from "react";
 
 export default function Form() {
   return (
-    <>
-      <h1>Welcome Back</h1>
-      <p>Welcome back! Please enter your details.</p>
-      <div>
+    <div className="bg-white px-10 py-20 rounded-3xl border-2 border-gray-200">
+      <h1 className="text-5xl font-semibold">Welcome Back</h1>
+      <p className="font-medium text-lg text-gray-500 mt-4">
+        Welcome back! Please enter your details.
+      </p>
+
+      {/* Email and Password */}
+      <div className="mt-8">
         <div>
-          <label>Email</label>
+          <label className="text-lg font-medium">Email</label>
           <input
-            className=""
+            className="w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent"
             placeholder="Enter your email"
             type="email"
           />
         </div>
-      </div>
-      <div>
         <div>
-          <label>Password</label>
+          <label className="text-lg font-medium">Password</label>
           <input
-            className=""
+            className="w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent"
             placeholder="Enter your password"
             type="password"
           />
         </div>
       </div>
-      <div>
+
+      {/* Remember and Forgot Password */}
+      <div className="mt-8 flex justify-between items-center border-2 border-green-700">
         <div>
           <input type="checkbox" name="" id="remember" />
-          <label for="remember">Remember for 30 days</label>
+          <label className="ml-2 font-medium text-base" for="remember">
+            Remember for 30 days
+          </label>
         </div>
+        <button className="font-medium text-base text-violet-500">
+          Forgot Password
+        </button>
       </div>
-    </>
+
+      {/* Login Button */}
+      <div className="mt-8 flex flex-col gap-y-4">
+        <button className="py-3 rounded-xl bg-violet-500 text-white text-lg font-bold">Sign in</button>
+        <button>Sign in with Google</button>
+      </div>
+    </div>
   );
 }
