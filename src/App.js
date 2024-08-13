7909eb8da6ad8f30afbf4acf260a63c085d4ea69
Septian Adi Pratama <septianadip7998@gmail.com>
2024-08-13 14:48:32 +0700

    border removal

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import Form from "./components/Form";
 
 function App() {
   return (
-    <div className="flex w-full h-screen border-4 border-red-500">
-      <div className="w-full flex items-center justify-center lg:w-1/2 border-4 border-blue-500">
+    <div className="flex w-full h-screen">
+      <div className="w-full flex items-center justify-center lg:w-1/2">
         <Form />
       </div>
-      <div className="hidden relative lg:flex h-full w-1/2 items-center justify-center bg-gray-200 border-4 border-green-500">
+      <div className="hidden relative lg:flex h-full w-1/2 items-center justify-center bg-gray-200">
         <div className="w-60 h-60 bg-gradient-to-tr from-violet-500 to-pink-500 rounded-full animate-bounce"></div>
-        <div className="w-full absolute bottom-0 h-1/2 bg-white/10 backdrop-blur-lg border-4 border-yellow-500"></div>
+        <div className="w-full absolute bottom-0 h-1/2 bg-white/10 backdrop-blur-lg "></div>
       </div>
     </div>
   );
