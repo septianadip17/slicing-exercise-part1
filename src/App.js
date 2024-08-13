124eb4d0a45c1bdbb8904aec0c80ec0a164f98ac
Septian Adi Pratama <septianadip7998@gmail.com>
2024-08-13 13:24:12 +0700

    screen slice to be 2

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import Form from "./components/Form";
 
 function App() {
   return (
-    <div>
-      <h1 className="text-5xl">Hello React</h1>
-      <Form />
+    <div className="flex w-full h-screen border-4 border-red-500">
+      <div className="w-full flex items-center justify-center lg:w-1/2 border-4 border-blue-500">
+        <Form />
+      </div>
+      <div className="hidden lg:flex h-full bg-gray-200">
+        <div>Hello</div>
+      </div>
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,5 @@
 import * as React from "react";
 
 export default function Form() {
-  return <div>Form</div>;
+  return <div>Hello Form</div>;
 }
